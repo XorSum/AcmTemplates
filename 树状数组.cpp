8993103b9978ef2cb15ba708d9b53c7d34807cc6
hamburger <1779754921@qq.com>
2018-07-28 11:30:49 +0800

    Create 树状数组.cpp

---
+++ 树状数组.cpp
@@ -0,0 +1,22 @@
+typedef long long ll;
+const ll maxn = 200005
+
+struct TreeArray{
+    ll a[maxn];
+    ll n=maxn;
+    void init(ll n){
+        this->n = n;
+        memset(a,0, sizeof(a));
+    }
+    inline ll lowbit(ll x){
+        return -x&x;
+    }
+    void add(ll x,ll v){
+        for (;x<=n;x+=lowbit(x)) a[x]+=v;
+    }
+    ll query(ll x){
+        ll ret = 0;
+        for (;x;x-=lowbit(x))  ret+=a[x];
+        return ret;
+    }
+};
