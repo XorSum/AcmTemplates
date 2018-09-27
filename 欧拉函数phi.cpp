1708d6f90dfe577d42152e9928ebf602e7ee7763
hamburger <1779754921@qq.com>
2018-09-27 21:41:20 +0800

    Create 欧拉函数phi.cpp

---
+++ 欧拉函数phi.cpp
@@ -0,0 +1,29 @@
+// 单个
+ll Euler(ll n) {
+    ll ret=n;
+    for(ll i=2;i<=sqrt(n);i++)
+    if(n%i==0)
+    {
+        ret=ret/i*(i-1);
+        while(n%i==0)
+          n/=i;
+    }
+    if(n>1)
+          ret=ret/n*(n-1);
+    return ret;
+}
+
+//所有
+const ll  maxn = 1000005;
+ll euler[maxn];
+void init() {
+    memset(euler,0,sizeof(euler));
+    euler[1]=1;
+    for(ll i=2; i<maxn; i++)
+        if(!euler[i])
+            for(ll j=i; j<maxn; j+=i) {
+                if(!euler[j])
+                    euler[j]=j;
+                euler[j]=euler[j]/i*(i-1);
+            }
+}
