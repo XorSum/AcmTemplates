ba77379d24d3f6f84f7793cfcf6602f90e0d3f8a
hamburger <1779754921@qq.com>
2018-10-04 17:34:10 +0800

    Update 最小费用流.cpp

--- 最小费用流.cpp
+++ 最小费用流.cpp
@@ -1,8 +1,7 @@
 const int MAXN = 100010;
 const int MAXM = 100010;
 const int INF  = 0x3f3f3f3f;
-struct Edge
-{
+struct Edge {
     int from, to, cap, flow, cost, next;
 };
 Edge edge[MAXM];
@@ -10,15 +9,14 @@ int head[MAXN], edgenum;
 int pre[MAXN];
 int dist[MAXN];
 bool vis[MAXN];
-void init()
-{
+
+void init() {
     edgenum = 0;
     memset(head, -1, sizeof(head));
 }
-void addEdge(int u, int v, int w, int c)
-{
+
+void addEdge(int u, int v, int w, int c){
   //  cout<<u<<" "<<v<<" "<<w<<" "<<c<<endl;
- 
     Edge E1 = {u, v, w, 0, c, head[u]};
     edge[edgenum] = E1;
     head[u] = edgenum++;
@@ -26,8 +24,8 @@ void addEdge(int u, int v, int w, int c)
     edge[edgenum] = E2;
     head[v] = edgenum++;
 }
-bool SPFA(int s, int t)
-{
+
+bool SPFA(int s, int t) {
     queue<int> Q;
     memset(dist, INF, sizeof(dist));
     memset(vis, false, sizeof(vis));
@@ -35,20 +33,16 @@ bool SPFA(int s, int t)
     dist[s] = 0;
     vis[s] = true;
     Q.push(s);
-    while(!Q.empty())
-    {
+    while(!Q.empty()) {
         int u = Q.front();
         Q.pop();
         vis[u] = false;
-        for(int i = head[u]; i != -1; i = edge[i].next)
-        {
+        for(int i = head[u]; i != -1; i = edge[i].next) {
             Edge E = edge[i];
-            if(dist[E.to] > dist[u] + E.cost && E.cap > E.flow)
-            {
+            if(dist[E.to] > dist[u] + E.cost && E.cap > E.flow) {
                 dist[E.to] = dist[u] + E.cost;
                 pre[E.to] = i;
-                if(!vis[E.to])
-                {
+                if(!vis[E.to]) {
                     vis[E.to] = true;
                     Q.push(E.to);
                 }
@@ -57,20 +51,18 @@ bool SPFA(int s, int t)
     }
     return pre[t] != -1;
 }
+
 void MCMF(int s, int t, int &cost, int &flow)
 {
     flow = 0;
     cost = 0;
-    while(SPFA(s, t))
-    {
+    while(SPFA(s, t)) {
         int Min = INF;
-        for(int i = pre[t]; i != -1; i = pre[edge[i^1].to])
-        {
+        for(int i = pre[t]; i != -1; i = pre[edge[i^1].to]){
             Edge E = edge[i];
             Min = min(Min, E.cap - E.flow);
         }
-        for(int i = pre[t]; i != -1; i = pre[edge[i^1].to])
-        {
+        for(int i = pre[t]; i != -1; i = pre[edge[i^1].to]){
             edge[i].flow += Min;
             edge[i^1].flow -= Min;
             cost += edge[i].cost * Min;
@@ -79,23 +71,13 @@ void MCMF(int s, int t, int &cost, int &flow)
     }
 }
 
-
-
-int main()
-{
+int main() {
     init();
-
     int s = 2*n+1,t =2*n+2;
-
     addEdge(from,t0,flow,cost);
-
     int cost,flow;
     MCMF(s,t,cost,flow);
- 
     printf("%d %d\n",flow,cost);
- 
     return 0;
 }
-
-
 
