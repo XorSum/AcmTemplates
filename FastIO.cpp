f0c200f165c51d4d30c4e11cca3534cf237720fd
hamburger <1779754921@qq.com>
2018-10-04 19:09:55 +0800

    Update FastIO.cpp

--- FastIO.cpp
+++ FastIO.cpp
@@ -26,3 +26,20 @@ namespace FastIO {
         return true;
     }
 } using FastIO::read;
+
+
+
+inline int read() {
+    char ch = getchar();  
+    int x = 0, f = 1;
+    while(ch < '0' || ch > '9') {
+        if(ch == '-') f = -1;
+        ch = getchar();
+    } 
+    while('0' <= ch && ch <= '9') {
+        x = x * 10 + ch - '0';
+        ch = getchar();
+    } 
+    return x * f;
+}
+
