df02f58bdc62a82f854119054ff4506b0922aac6
hamburger <1779754921@qq.com>
2018-10-12 16:46:16 +0800

    Update kmp.cpp

--- src/string/kmp.cpp
+++ src/string/kmp.cpp
@@ -30,3 +30,47 @@ int kmp_cnt(char  *s,int slen,char *t,int tlen,int *nxt){
     }
     return  cnt;
 }
+
+
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <cstring>
+const int maxn = 110;
+typedef long long ll;
+const int inf = 0x3f3f3f3f;
+using namespace std;
+void getNext(char *t,int len,int* nxt){
+    nxt[0]=-1;
+    int k=-1;
+    for (int i=1;i<len;i++){
+        while (k>-1&&t[k+1]!=t[i]) k=nxt[k];
+        if (t[k+1]==t[i]) k++;
+        nxt[i]=k;
+    }
+}
+int kmp(char *s,int slen,char* t,int tlen,int *nxt){
+    getNext(t,tlen,nxt);
+    int k=-1;
+    for (int i=0;i<slen;i++){
+        while (k>-1&&t[k+1]!=s[i]) k=nxt[k];
+        if (t[k+1]==s[i]) k++;
+        if (k==tlen-1){
+            return i-tlen+1;
+        }
+    }
+    return -1;
+}
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    char pat[100],str[100];
+    int nxt[100];
+    cin>>pat;
+    cin>>str;
+    int p = kmp(str,strlen(str),pat,strlen(pat),nxt);
+    cout<<p<<endl;
+    return 0;
+}
